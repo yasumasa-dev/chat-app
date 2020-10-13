4b089aea840db654378352c4efca6ee3c2c937eb
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-13 23:56:50 +0900

    deviseのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :users
+=======
+
+>>>>>>> parent of 7f59a58... usersテーブルの作成
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]

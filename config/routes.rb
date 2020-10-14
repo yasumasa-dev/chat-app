5f1e8e4b61ec457c011202d7a76668b0072135db
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-14 12:57:19 +0900

    ルーティング直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-=======
 
->>>>>>> parent of 7f59a58... usersテーブルの作成
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]

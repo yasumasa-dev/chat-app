517c3218e91f7a4dcf06aea232c2f7f8d8050a8c
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-28 23:58:10 +0900

    コメントアウト修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,10 +2,10 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
-
+#これにより、画像が存在しなければテキストが必要となり、画像があればテキストは不要になりました。
   validates :content, presence: true, unless: :was_attached?
-
   def was_attached?
     self.image.attached?
   end
+ #これにより、画像が存在しなければテキストが必要となり、画像があればテキストは不要になりました。
 end
